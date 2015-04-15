0aa53ba956c9448b0081e676e694f8238fc5dd53
Jacky Lee <aboutjax1990@gmail.com>
2015-04-16 02:21:34 +1200

    Update 2015-04-15T14:21:25.404Z

---
+++ scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jabselectsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jabselectsApp
+ */
+angular.module('jabselectsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
