cfe252159db08fdb8a64c95334c20b56f8d2b64d
Jacky Lee <aboutjax1990@gmail.com>
2015-04-16 00:53:52 +1200

    Add embedded latest playlist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,17 +20,18 @@ angular.module('jabselectsApp')
         redirect_uri: "http://example.com/callback.html",
       });
 
-    SC.get("/tracks", {
-      user_id: 5889536,
-      limit: 100
-  }, function (tracks) {
+  //   SC.get("/tracks", {
+  //     user_id: 5889536,
+  //     limit: 100
+  // }, function (tracks) {
 
-      for (var i = 0; i < tracks.length; i++) {
+  //     for (var i = 0; i < tracks.length; i++) {
 
-          console.log(tracks[i].title)
-      }
+  //       $('#player-track').append($('<li></li>').html(tracks[i].title));
 
-  });
+  //     }
+
+  // });
 
     SC.get("/playlists", {
         user_id: 55692119,
@@ -39,6 +40,7 @@ angular.module('jabselectsApp')
 
         for(var i = 0; i < playlists.length; i++) {
 
+            SC.oEmbed(playlists[0].uri, document.getElementById('player-playlist'));
             console.log(playlists[i].title)
         }
     });
